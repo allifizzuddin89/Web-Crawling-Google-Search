52d40732bd7d812342091030c18b5febadffd167
allifizzuddin89 <allifizzuddin@gmail.com>
2022-11-24 02:35:32 +0800

    log script

--- google_crawl/google_crawl/spiders/main.py
+++ google_crawl/google_crawl/spiders/main.py
@@ -1,6 +1,8 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+import logging
+from scrapy.utils.log import configure_logging
 
 
 class MainSpider(CrawlSpider):
@@ -12,9 +14,19 @@ class MainSpider(CrawlSpider):
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
+    configure_logging(
+        install_root_handler=False
+    )
+    logging.basicConfig(
+        filename="Logfile.log",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        filemode='w',
+        level=logging.DEBUG,
+    )
+
     def parse_item(self, response):
         item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
+        # item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        # item['name'] = response.xpath('//div[@id="name"]').get()
+        # item['description'] = response.xpath('//div[@id="description"]').get()
         return item
